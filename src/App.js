81eab124fba74acffbfb89647a7ad6f10113effd
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-09-04 20:48:03 +0530

    offering data add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Navbar from './components/Header/Navbar.js';
+import Footer from './components/Footer/Footer.js';
 import Home from './pages/home/Home.js'
 import Offerings from './pages/Offerings/Offerings.js'
 import About from './pages/about/About.js'
 import WhatsNew from './pages/whatsnew/WhatsNew.js';
 import ContactUs from './pages/contactus/ContactUs.js';
 
-
 import './App.css'
-import Navbar from './components/Header/Navbar.js';
-import Footer from './components/Footer/Footer.js';
 
 const App = () => {
   return (
--- src/pages/Offerings/Offerings.js
+++ src/pages/Offerings/Offerings.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import OfferingSlide from './OfferingSlide'
-import { client } from '../../client'
-
-
+import React, { useState, useEffect } from 'react';
+import OfferingSlide from './OfferingSlide';
+import { client } from '../../client';
 
 const Offerings = () => {
-
-  const [entry, setEntry] = useState([]);
+  const [parentEntries, setParentEntries] = useState([]);
 
   useEffect(() => {
     const fetchPage = async () => {
       try {
-        const response = await client.getEntries({
-          content_type: "offerings",
+        const parentResponse = await client.getEntries({
+          content_type: 'offerings',
+          "fields.type": 'Parent'
         });
-        console.log(response); // Check the response in the console
-        if (response.items.length) {
-          setEntry(response.items);
+        console.log(parentResponse); // Check the response in the console
+        if (parentResponse.items.length) {
+          setParentEntries(parentResponse.items);
         }
       } catch (error) {
         console.error(error);
@@ -27,20 +25,29 @@ const Offerings = () => {
   }, []);
 
   return (
-    <div className='Offerings'>
-      
-      {entry.map((item) => {
-        const { id, title, description } = item.fields
-        const image  = item.fields.image.fields.file.url
-        console.log(image)
-        return (
-          <OfferingSlide key={id} slideTitle={title} slideBg={image} slideDescription={description} />
-        )
-      })}
-    
-    </div>
+    <section className='offering'>
+      <div className='page_title'>
+        <h1>OFFERINGS</h1>
+      </div>
+      <div className='container'>
+        {parentEntries.map((parentItem) => {
+          const { id, title, description, image, subOfferings } = parentItem.fields;
+          const imageUrl = image.fields.file.url;
 
-  )
-}
+          return (
+            <div key={id} className='wrapper'>
+              <OfferingSlide
+                slideTitle={title}
+                slideBg={imageUrl}
+                slideDescription={description}
+                subOfferings={subOfferings}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Offerings
+export default Offerings;
