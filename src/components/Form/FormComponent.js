443880378c95c1d32d5bae272531938049826858
Vinod <keshariyavinod793@gmail.com>
2023-09-12 11:46:41 +0530

    changes styling

--- src/components/Form/FormComponent.js
+++ src/components/Form/FormComponent.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 
 const FormComponent = () => {
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <form>
-        <div className='d-flex'>
-          <div className='input-field'>
-            <label htmlFor='name'>Your Name *</label>
-            <input type='text' id='name' required />
+        <div className="d-flex">
+          <div className="input-field">
+            <label htmlFor="name">Your Name *</label>
+            <input type="text" id="name" required />
           </div>
-          <div className='input-field'>
-            <label htmlFor='email'>Your Email *</label>
-            <input type='email' id='email' required />
+          <div className="input-field">
+            <label htmlFor="email">Your Email *</label>
+            <input type="email" id="email" required />
           </div>
         </div>
-          <div className='input-field'>
-            <label htmlFor='service'>Select Service</label>
-            <select name='service' id='service'>
-              <option value='Agile coaching'>Agile coaching</option>
-              <option value='Agile transformation'>Agile transformation</option>
-              <option value='Agile training'>Agile training</option>
-              <option value='Change management leadership coaching'>Change management leadership coaching</option>
-              <option value='Team coaching'>Team coaching</option>
-              <option value='Leadership development'>Leadership development</option>
-            </select>
-          </div>
-        <div className='text-area'>
-          <label htmlFor='message'>Your Message</label>
-          <textarea name='message' id='message' cols='40' rows='10'></textarea>
+        <div className="input-field">
+          <label htmlFor="service">Select Service</label>
+          <select name="service" id="service">
+            <option value="Agile coaching">Agile coaching</option>
+            <option value="Agile transformation">Agile transformation</option>
+            <option value="Agile training">Agile training</option>
+            <option value="Change management leadership coaching">
+              Change management leadership coaching
+            </option>
+            <option value="Team coaching">Team coaching</option>
+            <option value="Leadership development">
+              Leadership development
+            </option>
+          </select>
         </div>
-        <input type='submit' value='Send' className='submit_btn' />
+        <div className="text-area">
+          <label htmlFor="message">Your Message</label>
+          <textarea name="message" id="message" cols="40" rows="10"></textarea>
+        </div>
+        <input type="submit" value="Send" className="submit_btn" />
       </form>
     </div>
   );
-}
+};
 
 export default FormComponent;
